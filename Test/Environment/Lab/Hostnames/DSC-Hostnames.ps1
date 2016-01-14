bbf3954bb0fb50603007f642ace0f0d99b525c25
David Yeo <d.yeo@imperial.ac.uk>
2016-01-14 18:01:04 +0000

    no message

---
+++ Test/Environment/Lab/Hostnames/DSC-Hostnames.ps1
@@ -0,0 +1,88 @@
+﻿configuration DSCLab_Hostnames
+{
+    Install-Module -Name xComputerManagement
+    Import-DscResource -ModuleName xComputerManagement
+
+Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename
+    {
+        xComputer NewName
+        { 
+            Name = $node.NodeName
+        }
+    }    
+    
+Node $AllNodes.Where{$_.Role -eq "Replica DC"}.Nodename
+    {
+        xComputer NewName
+        { 
+            Name = $node.NodeName
+        }
+    }
+    
+Node $AllNodes.Where{$_.Role -eq "Member Server"}.Nodename
+    {
+        xComputer NewName
+        { 
+            Name = $node.NodeName
+        }
+    }
+
+Node $AllNodes.Where{$_.Role -eq "Pull Server"}.Nodename
+    {
+        xComputer NewName
+        { 
+            Name = $node.NodeName
+        }
+    }
+}
+
+      
+$ConfigData = @{
+    AllNodes = @(
+                @{
+                    Nodename = "DSCLABDC01"
+                    Role = "Primary DC"
+                },
+        
+                @{
+                    Nodename = "DSCLABDC02"
+                    Role = "Replica DC"
+                },
+
+                @{
+                    Nodename = "DSCLABS01"
+                    Role = "Member Server"
+                },
+
+                @{
+                    Nodename = "DSCLABS02"
+                    Role = "Member Server"
+                },
+
+                @{
+                    Nodename = "DSCLABS03"
+                    Role = "Member Server"
+                },
+
+                @{
+                    Nodename = "DSCLABS04"
+                    Role = "Member Server"
+                },
+
+                @{
+                    Nodename = "DSCLABPULL01"
+                    Role = "Pull Server"
+                }
+            )
+        }
+
+
+DSCLab_Hostnames -ConfigurationData $ConfigData -OutputPath "C:\Users\dyeo\OneDrive - Imperial College London\DesiredStateConfiguration\Test\Environment\Lab\Hostnames\" -Verbose
+
+
+
+$Computername = $null
+$Computername = 'DSCLabDC01','DSCLabDC02','DSCLabS01','DSCLabS02','DSCLabS03','DSCLabS04','DSCLabPull01'
+
+#$creds = Get-Credential
+#Start-DscConfiguration -path "C:\Users\dyeo\OneDrive - Imperial College London\DesiredStateConfiguration\Test\Environment\Lab\Hostnames\" -wait -verbose -credential $Creds -force
---
+++ Test/Environment/Lab/LCM/DSClocalConfigManager-MOF.ps1
@@ -0,0 +1,24 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration LCMPUSH 
+{	
+	Node $computername
+	{
+		Settings
+		{
+			AllowModuleOverwrite = $True
+            ConfigurationMode = 'ApplyAndMonitor'
+			RefreshMode = 'Push'
+            RebootNodeIfNeeded = $True
+            ActionAfterReboot = 'ContinueConfiguration'
+		}
+	}
+}
+
+$Computername = $null
+$Computername = 'DSCLabDC01','DSCLabDC02','DSCLabS01','DSCLabS02','DSCLabS03','DSCLabS04'#,'DSCLabPull01'
+
+# Create the Computer.Meta.Mof in folder
+LCMPush -OutputPath "C:\Users\dyeo\OneDrive - Imperial College London\DesiredStateConfiguration\Test\Environment\Lab\LCM"
+
+#$creds = get-credential
+#Set-DscLocalConfigurationManager -ComputerName $computername -Path "C:\Users\dyeo\OneDrive - Imperial College London\DesiredStateConfiguration\Test\Environment\Lab\LCM" -Verbose -Credential $creds
