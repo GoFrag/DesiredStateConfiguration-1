ddd49c92cefde0979e5aa87940ccedaf2d301111
David Yeo <d.yeo@imperial.ac.uk>
2016-01-14 13:55:17 +0000

    no message

---
+++ DSClocalConfigManager-MOF.ps1
@@ -0,0 +1,33 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration LCMPUSH 
+{	
+	Node $Computername
+	{
+		Settings
+		{
+			AllowModuleOverwrite = $True
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+			RefreshMode = 'Pull'
+			RebootNodeIfNeeded = $True
+			ConfigurationID = $guid
+            RefreshFrequencyMins = 30
+		}
+
+        ConfigurationRepositoryWeb DSCHTTP {
+                #Name = 'DSCHTTP'
+                ServerURL = 'http://DSCLABPull01:8080/PSDSCPullServer.svc'
+                AllowUnsecureConnection = $true
+        }
+
+	}
+}
+
+$Computername = 'DSCLABDC01','DSCLABDC02','DSCLABPULL01'
+
+# Create the Computer.Meta.Mof in folder
+LCMPush -OutputPath c:\GIT\DesiredStateConfiguration\DSCResource\lab\LCM
+
+#$creds = Get-Credential
+#Set-DscLocalConfigurationManager -ComputerName $Computername -Path c:\GIT\DesiredStateConfiguration\DSCResource\lab\LCM -Verbose -Credential $creds
+
+#Enter-PSSession -ComputerName DSCLABDC1 -Credential DSCLABDC1\Administrator
