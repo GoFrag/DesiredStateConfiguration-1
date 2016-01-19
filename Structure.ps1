be0ebdfad48e00bb3148a70b051852b3aa350c3e
David J Yeo <david.j.yeo@outlook.com>
2016-01-19 13:07:09 +0000

    New Structure

---
+++ Structure.ps1
@@ -0,0 +1,112 @@
+﻿$sot = import-csv -path '\\ic\infrastructure\DSCResource\Setup\Directory Structure\SOT.csv'
+$sot.GetType().fullname
+$root = $null
+$root = "C:\Users\dyeo\OneDrive - Imperial College London\DesiredStateConfiguration"
+$ErrorActionPreference = "SilentlyContinue"
+
+##################################################################################
+### Where-object {$_.firstlevel} means eliminate any $null value in the column ###
+##################################################################################
+
+$Level1 = ($sot | select Firstlevel | Where-Object {$_.firstlevel})
+$Level2 = ($sot | select SecondLevel | Where-Object {$_.Secondlevel})
+$Level3 = ($sot | select ThirdLevel | Where-Object {$_.Thirdlevel})
+
+##########################################
+### Creates Top Level Folder Structure ###
+##########################################
+
+foreach ($level in $level1)
+    {
+        $CreationError = $null
+        Try
+            {
+                [string]$pathToCreateFirstLevel = $null
+                [string]$folderToCreateFirstLevel = $null
+                $folderToCreateFirstLevel = $level.FirstLevel
+                $pathToCreateFirstLevel = "$root\$folderToCreateFirstLevel"
+                If(!(Test-path -Path $pathToCreateFirstLevel)){
+                new-item -Path $pathToCreateFirstLevel -ItemType directory}
+                
+            else
+                
+                {
+                Write-Host "Do not need to create directory: $pathToCreateFirstLevel"
+                }
+            }
+
+        catch
+            
+            {
+                Write-Host "Unable to create directory: $pathToCreateFirstLevel"
+                Write-host $error[0].Exception
+            }
+   
+##############################################
+#### Creates Second Level Folder Structure ###
+##############################################
+    
+$FirstLevelRoot = $null
+$FirstLevelRoot = $pathToCreateFirstLevel 
+
+foreach ($secondLevel in $Level2)
+    {
+        Try
+            {
+                [string]$pathToCreateSecondLevel = $null
+                [string]$folderToCreateSecondLevel = $null
+                $folderToCreateSecondLevel = $secondLevel.SecondLevel
+                $pathToCreateSecondLevel = "$FirstLevelRoot\$folderToCreateSecondLevel"
+                If(!(Test-path -Path $pathToCreateSecondLevel)){
+                new-item -Path $pathToCreateSecondLevel -ItemType directory}
+                
+            else
+                
+                {
+                Write-Host "Do not need to create directory: $pathToCreateSecondLevel"
+                }
+            }
+
+        catch
+            
+            {
+                Write-Host "Unable to create directory: $pathToCreateSecondLevel"
+                Write-host $error[0].Exception
+            }
+
+############################################
+### Creates Third Level Folder Structure ###
+############################################
+
+$ThirdLevelRoot = $null
+$ThirdLevelRoot = $pathToCreateSecondLevel 
+
+foreach ($thirdLevel in $Level3)
+    {
+        Try
+            {
+                [string]$pathToCreateThirdLevel = $null
+                [string]$folderToCreateThirdLevel = $null
+                $folderToCreateThirdLevel = $ThirdLevel.ThirdLevel
+                $pathToCreateThirdLevel = "$ThirdLevelRoot\$folderToCreateThirdLevel"
+                If(!(Test-path -Path $pathToCreateThirdLevel)){
+                new-item -Path $pathToCreateThirdLevel -ItemType directory}
+                
+            else
+                
+                {
+                Write-Host "Do not need to create directory: $pathToCreateThirdLevel"
+                }
+            }
+
+        catch
+            
+            {
+                Write-Host "Unable to create directory: $pathToCreateThirdLevel"
+                Write-host $error[0].Exception
+            }
+        }
+    }
+}
+
+#End
---
+++ Testing/HTTP_PullServers/Script/LCMHTTPPull.ps1
@@ -0,0 +1,41 @@
+﻿[DSCLocalConfigurationManager()]
+Configuration LCMHTTPPull
+{
+    param
+        (
+            [Parameter(Mandatory=$true)]
+            [string[]]$ComputerName,
+
+            [Parameter(Mandatory=$true)]
+            [string]$guid
+        )      	
+	Node $ComputerName {
+	
+		Settings{
+		
+			AllowModuleOverwrite = $True
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+			RefreshMode = 'Pull'
+			ConfigurationID = $guid
+            }
+
+            ConfigurationRepositoryWeb DSCHTTP {
+            #Name = 'DSCHTTP'
+            ServerURL = 'http://DSCLABPull01:8080/PSDSCPullServer.svc'
+            AllowUnsecureConnection = $true
+            }
+		
+	    }
+    }
+
+# Computer list 
+$ComputerName='DSCLABDC01','DSCLABDC02','DSCLABS01','DSCLABS02','DSCLABS03','DSCLABS04'
+
+# Create Guid for the computers
+$guid=[guid]::NewGuid()
+
+# Create the Computer.Meta.Mof in folder
+LCMHTTPPull -ComputerName $ComputerName -Guid $guid -OutputPath C:\GIT\DesiredStateConfiguration\DSCResource\labrevised\MemberServers\PULLLCM\
+
+#$creds = Get-Credential
+#Set-DscLocalConfigurationManager -Path C:\GIT\DesiredStateConfiguration\DSCResource\labrevised\MemberServers\PULLLCM\ -Verbose -Credential $creds
